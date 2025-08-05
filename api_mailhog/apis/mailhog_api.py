f8cd0176f79b47d3e90c042a7f6d0be43f68069e
LineaRe <polina.reyner@gmail.com>
2025-08-05 19:24:21 +0300

    вынесли функции в классы клиенты

---
+++ api_mailhog/apis/mailhog_api.py
@@ -0,0 +1,19 @@
+import requests
+
+
+class MailhogApi:
+    def __init__(self, host, headers=None):
+        self.host = host
+        self.headers = headers
+
+
+
+    def get_api_v2_messages(self, limit=50):
+        params = {
+            'limit': limit,
+        }
+        """
+        Get Users emails
+        """
+        response = requests.get(url=f'{self.host}/api/v2/messages', params=params, verify=False)
+        return response
