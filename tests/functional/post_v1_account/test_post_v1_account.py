113d4a42809ad7c268321658cea9b98cc9470064
LineaRe <polina.reyner@gmail.com>
2025-08-05 19:37:03 +0300

    джоба для запуска тестов

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,12 +1,13 @@
 from json import loads
-
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
 
 
 def test_post_v1_account():
+
     # Регистрация пользователя
+
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
@@ -26,6 +27,7 @@ def test_post_v1_account():
 
     assert response.status_code == 201, f"Пользователь не был создан {response.json()}"
 
+
     # Получить письмо из почтового сервера
 
     response = mailhog_api.get_api_v2_messages(response)
@@ -33,12 +35,15 @@ def test_post_v1_account():
     print(response.text)
     assert response.status_code == 200, "Письмо не было получено"
 
+
     # Получить активационный токен
+
     token = get_activation_token_by_login(login, response)
 
     assert token is not None, f"Токен для пользователя {login} не был был получен"
 
     # Активация пользователя
+
     response = account_api.put_v1_account_token(token=token)
 
     print(response.status_code)
@@ -46,7 +51,6 @@ def test_post_v1_account():
     assert response.status_code == 200, "Пользователь не был активирован"
 
     # Авторизоваться
-
 
     json_data = {
         'login': login,
